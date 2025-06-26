a2e596082b51170fa8f4967e990e918b612e12b6
Jdaans <59904219+Jdaans@users.noreply.github.com>
2025-06-26 00:14:38 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import Links from './components/Links'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div className="container">
+
+      <Header />
+      <Links />
+
+    </div>
+
+
+  )
+}
+
+export default App
